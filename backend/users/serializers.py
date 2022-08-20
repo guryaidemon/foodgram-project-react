36e1768ea86a82420b6da2b0abe063916e684d3f
Dmitry <guryaidemon@gmail.com>
2022-08-20 16:23:36 +0300

    create serializers for Follow

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,7 +2,7 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from backend.users.models import CustomUser
+from backend.users.models import CustomUser, Follow
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -30,3 +30,24 @@ class CustomUserCreateSerializer(UserCreateSerializer):
             'first_name': {'required': True},
             'last_name': {'required': True},
         }
+
+
+class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed'
+        )
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=obj.id).exists()

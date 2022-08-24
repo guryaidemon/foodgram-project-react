f8521c1e77c7f8bf44661de509839f88ca660954
Dmitry <guryaidemon@gmail.com>
2022-08-24 14:31:10 +0300

    delete not used imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,14 +6,11 @@ from djoser.views import UserViewSet
 from rest_framework import status, viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (
-    AllowAny,
     IsAuthenticated,
     IsAuthenticatedOrReadOnly
 )
 from rest_framework.response import Response
-from rest_framework.serializers import ListSerializer
 
 from api.mixins import PermissionAndPaginationMixin
 from api.permissions import AuthorOrReadOnly

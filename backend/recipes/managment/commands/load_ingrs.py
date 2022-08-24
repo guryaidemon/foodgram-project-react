d3e24cb256d3c69694122721d6e0886b64246b7d
Dmitry <guryaidemon@gmail.com>
2022-08-24 14:00:24 +0300

    fix bugs

--- backend/recipes/managment/commands/load_ingrs.py
+++ backend/recipes/managment/commands/load_ingrs.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         data_path = settings.BASE_DIR
         with open(
-            f'(data_path)/data/ingridients.csv',
+            f'{data_path}/data/ingredients.csv',
             'r',
             encoding='utf-8'
         ) as file:
